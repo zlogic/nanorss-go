8c3ffb72a293a5e2a04a5d1d94fa18e6b212b1cb
Dmitry Zolotukhin <zlogic@gmail.com>
2019-04-19 20:58:47 +0200

    Disabled caching for the API.

--- server/router.go
+++ server/router.go
@@ -7,23 +7,32 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// NoCacheHeaderMiddlewareFunc creates a handler to disable caching.
+func NoCacheHeaderMiddlewareFunc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "private")
+		next.ServeHTTP(w, r)
+	})
+}
+
 // CreateRouter returns a router and all handlers.
 func CreateRouter(s *Services) (*mux.Router, error) {
 	r := mux.NewRouter()
-	r.HandleFunc("/", RootHandler(s)).Methods("GET")
-	r.HandleFunc("/login", HTMLLoginHandler(s)).Methods("GET")
-	r.HandleFunc("/logout", LogoutHandler(s)).Methods("GET")
-	r.HandleFunc("/feed", HTMLFeedHandler(s)).Methods("GET").Name("feed")
-	r.HandleFunc("/settings", HTMLSettingsHandler(s)).Methods("GET").Name("settings")
+	r.HandleFunc("/", RootHandler(s)).Methods(http.MethodGet)
+	r.HandleFunc("/login", HTMLLoginHandler(s)).Methods(http.MethodGet)
+	r.HandleFunc("/logout", LogoutHandler(s)).Methods(http.MethodGet)
+	r.HandleFunc("/feed", HTMLFeedHandler(s)).Methods(http.MethodGet).Name("feed")
+	r.HandleFunc("/settings", HTMLSettingsHandler(s)).Methods(http.MethodGet).Name("settings")
 	r.HandleFunc("/favicon.ico", FaviconHandler)
 	fs := http.FileServer(staticResourceFileSystem{http.Dir("static")})
 	r.PathPrefix("/static/").Handler(http.StripPrefix(strings.TrimRight("/static", "/"), fs))
 
 	api := r.PathPrefix("/api").Subrouter()
-	api.HandleFunc("/login", LoginHandler(s)).Methods("POST")
-	api.HandleFunc("/configuration", SettingsHandler(s)).Methods("GET", "POST")
-	api.HandleFunc("/feed", FeedHandler(s)).Methods("GET")
-	api.HandleFunc("/items/{key}", FeedItemHandler(s)).Methods("GET")
-	api.HandleFunc("/refresh", RefreshHandler(s)).Methods("GET")
+	api.Use(NoCacheHeaderMiddlewareFunc)
+	api.HandleFunc("/login", LoginHandler(s)).Methods(http.MethodPost)
+	api.HandleFunc("/configuration", SettingsHandler(s)).Methods(http.MethodGet, http.MethodPost)
+	api.HandleFunc("/feed", FeedHandler(s)).Methods(http.MethodGet)
+	api.HandleFunc("/items/{key}", FeedItemHandler(s)).Methods(http.MethodGet)
+	api.HandleFunc("/refresh", RefreshHandler(s)).Methods(http.MethodGet)
 	return r, nil
 }
