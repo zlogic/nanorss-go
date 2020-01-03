bbfe05300153343eecd2d1022d904180616fcbe3
Dmitry Zolotukhin <zlogic@gmail.com>
2020-01-03 20:37:11 +0100

    Bumped up dependency versions. Using in-memory Badger for tests.

--- data/data_test.go
+++ data/data_test.go
@@ -1,48 +1,18 @@
 package data
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
-	"testing"
-
 	"github.com/dgraph-io/badger/v2"
 	log "github.com/sirupsen/logrus"
 )
 
 var dbService *DBService
 
-func TestMain(m *testing.M) {
-	dir, err := ioutil.TempDir("", "nanorss")
-	if err != nil {
-		panic(fmt.Sprintf("cannot create tempdir %v", err))
-	}
-	err = createDb(dir)
-	if err != nil {
-		panic(fmt.Sprintf("cannot open database %v", err))
-	}
-
-	code := m.Run()
-	destroyDb(dir)
-	os.Exit(code)
-}
-
-func createDb(dir string) (err error) {
-	var opts = badger.DefaultOptions(dir)
+func resetDb() (err error) {
+	var opts = badger.DefaultOptions("")
 	opts.Logger = log.New()
 	opts.ValueLogFileSize = 1 << 20
-	opts.SyncWrites = false
-	opts.CompactL0OnClose = false
+	opts.InMemory = true
 
 	dbService, err = Open(opts)
 	return
-}
-
-func resetDb() error {
-	return dbService.db.DropAll()
-}
-
-func destroyDb(dir string) {
-	dbService.Close()
-	os.RemoveAll(dir)
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.13
 
 require (
 	github.com/DataDog/zstd v1.4.4 // indirect
-	github.com/dgraph-io/badger/v2 v2.0.0
+	github.com/dgraph-io/badger/v2 v2.0.1
 	github.com/dgraph-io/ristretto v0.0.1 // indirect
 	github.com/dgryski/go-farm v0.0.0-20191112170834-c2139c5d712b // indirect
 	github.com/golang/protobuf v1.3.2 // indirect
@@ -20,9 +20,9 @@ require (
 	github.com/ssor/bom v0.0.0-20170718123548-6386211fdfcf // indirect
 	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/stretchr/testify v1.4.0
-	golang.org/x/crypto v0.0.0-20191206172530-e9b2fee46413
+	golang.org/x/crypto v0.0.0-20191227163750-53104e6ec876
 	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553
-	golang.org/x/sys v0.0.0-20191210023423-ac6580df4449 // indirect
+	golang.org/x/sys v0.0.0-20200103143344-a1369afcdac7 // indirect
 	golang.org/x/text v0.3.2 // indirect
 	gopkg.in/h2non/gock.v1 v1.0.15
 )
